9665b392cc79453d6069f8dcd7fb4a5f128fa1eb
Rafael Vieira <rafaelvieira1720@gmail.com>
2026-07-02 21:58:34 -0300

    feat: integrate @vieira/analytics for tracking page views, add VieiraAnalytics component to main application, and update package.json to include local vendor dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { VieiraAnalytics } from '@vieira/analytics/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
@@ -6,5 +7,6 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <VieiraAnalytics projectKey="gravacoes-treinamento" />
   </StrictMode>,
 )
